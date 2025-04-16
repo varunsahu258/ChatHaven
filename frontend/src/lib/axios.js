8fa1fe65c060623a3b7350385bb265d8b72a935d
varunsahu258 <74762597+varunsahu258@users.noreply.github.com>
2025-04-17 01:03:27 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,4 @@
-import axios from "axios";
-
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
