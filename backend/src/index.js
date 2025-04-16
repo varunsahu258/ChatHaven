8f65151413e8e86416bf9299433a5e68a6ba20f2
varunsahu258 <74762597+varunsahu258@users.noreply.github.com>
2025-04-17 01:17:26 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chathaven-zvsp.onrender.com",
+    origin: "https://chathaven-gold.vercel.app",
     credentials: true,
   })
 );
