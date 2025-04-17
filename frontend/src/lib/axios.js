7b6d94a636acdd5b97e06be57528bf747cc58023
varunsahu258 <varunsahu258@gmail.com>
2025-04-17 13:16:16 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
-const instance = axios.create({
-  baseURL: 'https://chathaven-zvsp.onrender.com',
-  withCredentials: true, 
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
-
-export default instance;
