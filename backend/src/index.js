059704ddcf61c708ec706c94d58e7f9d509599ab
varunsahu258 <varunsahu258@gmail.com>
2025-04-17 13:11:08 +0530

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { Server } from 'socket.io';
 
 import path from "path";
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,  
-  withCredentials: true,
+const instance = axios.create({
+  baseURL: 'https://chathaven-zvsp.onrender.com',
+  withCredentials: true, 
 });
+
+export default instance;
